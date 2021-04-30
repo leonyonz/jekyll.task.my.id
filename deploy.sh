40478b6558ad7e3aaf1c193e91f4ab17775ee53c
root <leonachild@gmail.com>
2021-04-30 18:08:41 +0700

    post tes

---
+++ deploy.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+echo "Build Jekyll !!!"
+
+echo ""
+
+jekyll build;
+
+echo "Proses Deploy !!!"
+echo ""
+
+read -p "Masukan Commit : " commit;
+
+git add .
+
+git commit -m "$commit";
+
+git push
+
+echo "Deploy Website !!!"
+
+cd _site
+
+git add . 
+
+git commit -m "$commit"
+
+git push origin master
+
+echo ""
+echo "SELESAI !!"
--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20210430.1849';
+const cacheName = 'chirpy-20210430.1908';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
